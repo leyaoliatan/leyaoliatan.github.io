39d626ffba645e339ff01801d80e2b198beb263e
Leyao (Lia) Tan <leyao.lia.tan@gmail.com>
2025-02-10 15:38:45 -0800

    add piv

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -5,32 +5,32 @@ const timelineData = [
   {
     period: '1999-2000',
     location: 'Huangshuang Village',
-    image: '/images/timeline/huangshuang.jpg',
+    image: `${process.env.PUBLIC_URL}/images/timeline/huangshuang.jpg`,
   },
   {
     period: '2000-2013',
     location: 'Zixing County',
-    image: '/images/timeline/zixing.jpg',
+    image: `${process.env.PUBLIC_URL}/images/timeline/zixing.jpg`,
   },
   {
     period: '2013-2016',
     location: 'Chenzhou City',
-    image: '/images/timeline/chenzhou.jpg',
+    image: `${process.env.PUBLIC_URL}/images/timeline/chenzhou.jpg`,
   },
   {
     period: '2016-2020',
     location: 'Beijing',
-    image: '/images/timeline/beijing.jpg',
+    image: `${process.env.PUBLIC_URL}/images/timeline/beijing.jpg`,
   },
   {
     period: '2020-2022',
     location: 'Shanghai',
-    image: '/images/timeline/shanghai.jpg',
+    image: `${process.env.PUBLIC_URL}/images/timeline/shanghai.jpg`,
   },
   {
     period: '2022-Now',
     location: 'Vancouver',
-    image: '/images/timeline/vancouver.jpg',
+    image: `${process.env.PUBLIC_URL}/images/timeline/vancouver.jpg`,
   },
 ];
 
--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,14 +3,14 @@ const degrees = [
     school: 'University of British Columbia',
     degree: 'Ph.D. Student in Quantitative Marketing',
     link: 'https://www.sauder.ubc.ca/',
-    year: 2023,
+    year: '2023-',
     notes: 'Sauder School of Business',
   },
   {
     school: 'Shanghai Jiao Tong University',
     degree: 'M.S. in Quantitative Marketing',
     link: 'https://www.acem.sjtu.edu.cn/en/',
-    year: 2023,
+    year: '2020-2023',
     notes: 'GPA: 3.93/4.00 | Rank: 1/82',
   },
   {
@@ -31,7 +31,7 @@ const degrees = [
     school: 'Renmin University of China',
     degree: "Bachelor's in Politics, Philosophy, and Economics (PPE)",
     link: 'https://www.ruc.edu.cn/en',
-    year: 2020,
+    year: '2016-2020',
     notes: 'GPA: 3.85/4.00 | Rank: 2/31 | National Scholarship Recipient',
   },
 ];
